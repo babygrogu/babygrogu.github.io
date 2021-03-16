4d7244c07f57cb6d1115f40aa286d487936321a9
PENGU1NS <DROHAMBARCHIAN@GMAIL.COM>
2021-03-16 03:06:06 -0700

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'March 14, 2021',
+  birthdate: 'March 14, 2022',
   name: 'Victoria'
 };
 
